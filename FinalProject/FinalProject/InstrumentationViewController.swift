d5edafac6d29f1798e03faa127bd49b7a512a6f0
rylad <svalois@100366mbp15.local>
2017-04-23 10:15:38 -0400

    Minor Update

--- FinalProject/FinalProject/InstrumentationViewController.swift
+++ FinalProject/FinalProject/InstrumentationViewController.swift
@@ -78,7 +78,11 @@ class InstrumentationViewController: UIViewController, UITextFieldDelegate {
     //MARK: Misc button handling
     @IBAction func refreshRate(_ sender: UISlider) {
         engine.refreshTimer?.invalidate()
-        engine.refreshRate = Double(Double(sender.value))
+        print (sender.value)
+        print (Double(sender.value))
+        print (Double(Double(sender.value)))
+        //engine.refreshRate = Double(Double(sender.value))
+        engine.refreshRate = Double(sender.value)
     }
 
     @IBAction func toggleOn(_ sender: UISwitch) {
